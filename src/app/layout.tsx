079425f5bcec82cd21f929ca170e3499c1a89928
barlennan2bb <barlennan2bb@gmail.com>
2024-08-22 15:31:11 -0500

    per seo reco

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,32 @@
 import "./globals.css";
-// import React from "react";
-
 import Navbar from "@/components/navbar/Navbar";
 import styles from "./layout.module.css";
 import dynamic from "next/dynamic";
-// import Footer from "@/components/footer/Footer";
-const Footer = dynamic(() => import("@/components/footer/footer"));
-
 import { Inter } from "next/font/google";
-import "./globals.css";
+
+const Footer = dynamic(() => import("@/components/footer/Footer"));
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
-    default: "Next.js 14 Homepage",
-    template: "%s | Next.js 14",
+    default: "AceMQ Consulting | Expert RabbitMQ Messaging & Queuing Solutions",
+    template: "%s | AceMQ",
   },
-  description: "Next.js starter app description",
+  description: "AceMQ Consulting specializes in RabbitMQ solutions, including consulting, support, and professional services. Optimize your systems with our expert RabbitMQ services.",
 };
 
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <ClientSideProviderTest> */}
-        <div className="container">
+        <div className={styles.container}>
           <Navbar />
-          <div className={styles.container}></div>
-          {children}
+          <div className="container">{children}</div>
           <Footer />
         </div>
-        {/* </ClientSideProviderTest> */}
       </body>
     </html>
   );
 }
 
-// to do today
-// zip extract of parallel distributed
-// acemq initialization and development home page adjustments
-
-// Initialized Acemq Website with Nextjs
-// Delveloped Half of the Acemq landing page from scratch
-// -- 1  done
-// deployed Acemq to vercel
-// Initialized a git repositories for acemq and ace8 -- done half
-// 30 min next tetorial
